11a22189b35828223b28dc68083f2650e5fa1734
yverenoir <yuechih@gmail.com>
2023-06-13 22:50:36 +0800

    feat: add message for insufficient fund in vault

--- src/const/messages.ts
+++ src/const/messages.ts
@@ -1,7 +1,8 @@
 export const messages = {
-    amountInsufficient: "Your requested amount is bigger than the allowed withdrawal amount",
+    withdrawThresholdExceeded: "Your requested amount is bigger than the allowed withdrawal amount",
+    amountInsufficient: "The ATM is not able to dispense the requested amount",
     overdraft: "Your requested amount is bigger than your balance, you will be going into minus after this withdrawal",
-    withdrawUnsuccessful: "Your requested amount cannot be withdrawn",
+    withdrawUnsuccessful: "Something went wrong, your requested amount cannot be dispensed",
     illegalAmount: "We can only dispense a multiple of 5, please chose a different amount",
     wrongPin: "The PIN entered is wrong, please try again",
 }
