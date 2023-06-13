11a543d39b5266bd2b8f751f5e618e7f8c06b7d8
yverenoir <yuechih@gmail.com>
2023-06-13 18:51:12 +0800

    feat: add card container around content

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
-import {AppBar, Toolbar} from "@mui/material";
+import {AppBar, Box, Grid, Toolbar} from "@mui/material";
 import logo from "./logo.png";
 
 function Layout(props: any) {
 
     return (
-        <div>
+        <div style={{textAlign: "center"}}>
             <AppBar position={"static"}>
                 <Toolbar>
                     <img className={"logo"} src={logo}/>
                 </Toolbar>
             </AppBar>
-            <div>{props.children}</div>
+            <Box p={5}></Box>
+            <Grid container direction={"column"} alignItems={"center"}>
+                <Grid item>
+                    <Box sx={{backgroundColor: '#F0EEF4', borderRadius: '32px', width: '787px'}}>
+                        <Box p={5}>{props.children}</Box>
+                    </Box>
+                </Grid>
+            </Grid>
         </div>
     );
 }
